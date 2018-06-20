a519b216dc33c833c51df3209e50922ba2800aba
Alexandru Solomes <alexandrusolomes@Alexandrus-MacBook-Pro.local>
2018-06-21 00:50:36 +0100

    allow_redirects=False in http request

--- Standard_scraper/src/EveningScraper.py
+++ Standard_scraper/src/EveningScraper.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import requests
 from bs4 import BeautifulSoup
 import pickle
@@ -23,7 +23,7 @@ def _random_sleep(minimum=3, maximum=10, sd=1):
             value = maximum + np.random.uniform(0, 1)
         time.sleep(value)
 
-        
+
 def make_request(url, method, **kwargs):
     """
     <to add>
@@ -32,8 +32,8 @@ def make_request(url, method, **kwargs):
         response = requests.post(url, **kwargs)
     else:
         response = requests.get(url, **kwargs)
-    
-    # check for json 
+
+    # check for json
     if 'html' or 'text' in response.headers['Content-Type']:
         content = response.content
         response.close()
@@ -43,8 +43,8 @@ def make_request(url, method, **kwargs):
         return None
 
 def article_scraper(url, identifier):
-    
-    soup = make_request(url=url, method='get')
+
+    soup = make_request(url=url, method='get', allow_redirects=False)
     if soup:
         _random_sleep(
             minimum=1,
@@ -53,32 +53,32 @@ def article_scraper(url, identifier):
         )
 
         title = soup.find('h1', attrs={'class':'headline'}).text.strip()
-        
+
         body = soup.find('div', attrs={'class':'body-content'})
         tag_box = soup.find('aside', attrs={'class':'tags'})
-        
+
         if (body, tag_box):
             text = [x.text.strip() for x in body.findAll('p')]
             text = ''.join(x for x in text)
-            
+
             tags = [tag.text.strip() for tag in tag_box.findAll('a')]
-            
+
             answer_dict = {
                 "Title" : title,
                 "Link" : url,
                 "Text" : text,
                 "Tags" : tags
             }
-            return answer_dict 
-        
+            return answer_dict
+
 def save_output(identifier, file_dict):
-    
+
     file_save_name = 'labeled_newspaper_articles.pickle'
-    
+
     if os.path.isfile(output_path + file_save_name):
         with open(output_path + file_save_name, 'rb') as handle:
             existing_files = pickle.load(handle)
-        
+
         if identifier not in existing_files.keys():
             existing_files.update({identifier : file_dict})
             with open(output_path + file_save_name, 'wb') as handle:
@@ -87,7 +87,7 @@ def save_output(identifier, file_dict):
         with open(output_path + file_save_name, 'wb') as handle:
             articles = {identifier : file_dict}
             pickle.dump(articles, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
+
 with open('../src/evening_standard_id_link_dict.pickle', 'rb') as handle:
     master_dict = pickle.load(handle)
 
