cd60f84b58b7bbb8396917ca0a80d9275a593383
alexandru solomes <mariussolomes@gmail.com>
2018-06-22 09:27:52 +0100

    prints

--- Standard_scraper/src/EveningScraper.py
+++ Standard_scraper/src/EveningScraper.py
@@ -106,6 +106,9 @@ for ID, partial_extension in list(master_dict.items()):
             data_dict = article_scraper('https://www.standard.co.uk' + partial_extension, ID)
             if data_dict:
                 save_output(ID, data_dict)
+                print('saved!')
+            else:
+                print('no data_dict...')
         else:
             print('already_done')
             pass
